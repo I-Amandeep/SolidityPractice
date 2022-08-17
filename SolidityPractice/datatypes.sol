c40e369f758cce4ca501d628792bf6feb0f16543
Remix Workspace <>
2022-08-17 16:03:39 +0530

    Practice

---
+++ SolidityPractice/datatypes.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+
+//datatypes are of two types- values, references
+//values
+contract valueType
+{  
+    bool public boolTemp=true;
+    uint public temp=243;
+    int public  temp2=143;
+    address public addrss=0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    bytes public b3;  //there is no concept of null in solidity.
+
+}
